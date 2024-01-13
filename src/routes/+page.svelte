cee66fe142566943dee15cf9e14d11e213aa0bc3
idomshi <56523771+idomshi@users.noreply.github.com>
2024-01-13 13:50:33 +0900

    add home page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<header>
+	<h1>Checkerd</h1>
+	<p>チェック柄画像生成</p>
+
+	<p>Checkeredはチェック柄画像を生成するツールです。</p>
+	<p>URLをパラメータにしてSVG形式の画像を自由に作り出すことができます。</p>
+</header>
+
+<section>
+	<h2>sample</h2>
+	<div>
+		<h3>01</h3>
+		<img
+			alt="sample checkerd 01"
+			src="/128x128/003069/0054b666h64-32_0054b666v64-32_fff6v4-32_fff6h4-32_fad93f80v2-92_fad93f80v2-100_fad93f80h2-92_fad93f80h2-100/320x240.svg"
+		/>
+		<a
+			href="/128x128/003069/0054b666h64-32_0054b666v64-32_fff6v4-32_fff6h4-32_fad93f80v2-92_fad93f80v2-100_fad93f80h2-92_fad93f80h2-100/1920x1080.svg"
+			>1920x1080</a
+		>
+		<a
+			href="/128x128/003069/0054b666h64-32_0054b666v64-32_fff6v4-32_fff6h4-32_fad93f80v2-92_fad93f80v2-100_fad93f80h2-92_fad93f80h2-100/800x600.svg"
+			>800x600</a
+		>
+	</div>
+	<div>
+		<h3>02</h3>
+		<img alt="sample checkerd 02" src="/40x40/fcfcfc/ef000080h20-20_ef000080v20-20/320x240.svg" />
+		<a href="/40x40/fcfcfc/ef000080h20-20_ef000080v20-20/1920x1080.svg">1920x1080</a>
+		<a href="/40x40/fcfcfc/ef000080h20-20_ef000080v20-20/800x600.svg">800x600</a>
+	</div>
+</section>
