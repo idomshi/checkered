a95767f04e5a479d1324f7a809dae33899b35b2c
idomshi <56523771+idomshi@users.noreply.github.com>
2024-01-14 10:28:57 +0900

    split into Gallery component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import Generator from '$lib/Generator.svelte';
 	import Spec from '$lib/Spec.svelte';
+	import Gallery from '$lib/Gallery.svelte';
 </script>
 
 <header>
@@ -15,31 +16,7 @@
 
 <Generator />
 <Spec />
-
-<section>
-	<h2>sample</h2>
-	<div>
-		<h3>01</h3>
-		<img
-			alt="sample checkerd 01"
-			src="/128x128/003069/0054b666h64-32_0054b666v64-32_fff6v4-32_fff6h4-32_fad93f80v2-92_fad93f80v2-100_fad93f80h2-92_fad93f80h2-100/320x240.svg"
-		/>
-		<a
-			href="/128x128/003069/0054b666h64-32_0054b666v64-32_fff6v4-32_fff6h4-32_fad93f80v2-92_fad93f80v2-100_fad93f80h2-92_fad93f80h2-100/1920x1080.svg"
-			>1920x1080</a
-		>
-		<a
-			href="/128x128/003069/0054b666h64-32_0054b666v64-32_fff6v4-32_fff6h4-32_fad93f80v2-92_fad93f80v2-100_fad93f80h2-92_fad93f80h2-100/800x600.svg"
-			>800x600</a
-		>
-	</div>
-	<div>
-		<h3>02</h3>
-		<img alt="sample checkerd 02" src="/40x40/fcfcfc/ef000080h20-20_ef000080v20-20/320x240.svg" />
-		<a href="/40x40/fcfcfc/ef000080h20-20_ef000080v20-20/1920x1080.svg">1920x1080</a>
-		<a href="/40x40/fcfcfc/ef000080h20-20_ef000080v20-20/800x600.svg">800x600</a>
-	</div>
-</section>
+<Gallery />
 
 <style>
 	:global(*) {
@@ -49,18 +26,12 @@
 		line-height: 1.7rem;
 	}
 
-	h1,
-	h2,
-	h3 {
+	h1 {
 		margin-top: 1rem;
 	}
 
 	header {
 		margin: 1rem;
-	}
-
-	section {
-		margin: 0 1rem;
 	}
 
 	.title {
